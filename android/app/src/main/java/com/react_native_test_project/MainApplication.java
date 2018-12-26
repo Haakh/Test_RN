7492790814d90567e3c071d1b59b6b6a486adf86
haakh <hakbhavish@gmail.com>
2018-12-26 18:17:03 +0530

    init

---
+++ android/app/src/main/java/com/react_native_test_project/MainApplication.java
@@ -0,0 +1,49 @@
+package com.react_native_test_project;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.segment.analytics.reactnative.integration.amplitude.RNAnalyticsIntegration_AmplitudePackage;
+import com.segment.analytics.reactnative.core.RNAnalyticsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNAnalyticsIntegration_AmplitudePackage(),
+            new RNAnalyticsPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
