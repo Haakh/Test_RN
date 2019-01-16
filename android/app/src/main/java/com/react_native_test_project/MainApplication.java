ea9384d217cf6d4a532d50088e1939d7abc03a1c
bhavish <bhavish@getpulse.care>
2019-01-16 18:39:10 +0530

    test

--- android/app/src/main/java/com/react_native_test_project/MainApplication.java
+++ android/app/src/main/java/com/react_native_test_project/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_test_project;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.segment.analytics.reactnative.integration.amplitude.RNAnalyticsIntegration_AmplitudePackage;
 import com.segment.analytics.reactnative.core.RNAnalyticsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNAnalyticsIntegration_AmplitudePackage(),
             new RNAnalyticsPackage()
       );
